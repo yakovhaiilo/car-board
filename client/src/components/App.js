d3520cd3a1c416691acb257f71fd12b167c7ca50
Yakov Haiilo <yakovhailo123@gmail.com>
2020-08-16 20:42:26 +0300

    refactoring

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CarBoardApp from './CarBoardApp'
+
+
+function App () {
+  return(<div className = 'App'>
+             <CarBoardApp/>
+        </div>
+  ) 
+}
+
+export default App;
